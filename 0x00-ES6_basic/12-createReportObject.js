5e79773a242a0c5d6383a2fe577bff23459683fa
MercyKorir <mcthegreat78@gmail.com>
2023-06-27 13:25:42 +0300

    I have added the file testing for method properties and spread operator

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  const newObject = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+  return newObject;
+}
