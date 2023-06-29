be9169f16f142bc81efc559f9b198608631635a1
MercyKorir <mcthegreat78@gmail.com>
2023-06-30 02:19:46 +0300

    I have added the file for 2-hbtn_course.js

--- 0x02-ES6_classes/index.js
+++ 0x02-ES6_classes/index.js
@@ -1,21 +1,32 @@
-class Cat {
-  constructor(name) {
-    this.name = name;
-  }
+// class Cat {
+//   constructor(name) {
+//     this.name = name;
+//   }
 
-  speak() {
-    console.log(`${this.name} makes a noise.`);
-  }
+//   speak() {
+//     console.log(`${this.name} makes a noise.`);
+//   }
+// }
+
+// class Lion extends Cat {
+//   speak() {
+//     super.speak(); //references speak() method in parent class
+//     console.log(`${this.name} roars.`);
+//   }
+// }
+
+// const l = new Lion('Simba');
+// l.speak();
+// // Fuzzy makes a noise.
+// // Fuzzy roars.
+
+const firstName = 'Mercy';
+const no = 24;
+const arr = ['Mercy', 25];
+
+console.log(typeof firstName === 'string');
+console.log(typeof no === 'number');
+console.log(Array.isArray(arr));
+for (const item of arr) {
+  console.log(typeof item === 'string');
 }
-
-class Lion extends Cat {
-  speak() {
-    super.speak(); //references speak() method in parent class
-    console.log(`${this.name} roars.`);
-  }
-}
-
-const l = new Lion('Simba');
-l.speak();
-// Fuzzy makes a noise.
-// Fuzzy roars.
