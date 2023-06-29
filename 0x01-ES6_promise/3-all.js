48aaa37c87596a3380496d6268f06224e12c6d4c
MercyKorir <mcthegreat78@gmail.com>
2023-06-29 18:36:09 +0300

    I have added the test for Promise.all method

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,29 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const uploadPhotoPromise = uploadPhoto();
+  const createUserPromise = createUser();
+  let text = '';
+  let count = 0;
+
+  Promise.all([uploadPhotoPromise, createUserPromise])
+    .then((values) => {
+      for (const val of values) {
+        const objVal = Object.values(val);
+        for (const x of objVal) {
+          count += 1;
+          if (x !== 200) {
+            if (count !== 4) {
+              text += `${x} `;
+            } else {
+              text += x;
+            }
+          }
+        }
+      }
+      console.log(text);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
