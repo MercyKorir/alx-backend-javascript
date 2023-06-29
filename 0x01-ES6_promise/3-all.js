309ab490c71b51a1bb8692ae3fe33695e7896f69
MercyKorir <mcthegreat78@gmail.com>
2023-06-29 19:27:55 +0300

    Made some changes to the file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,15 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const uploadPhotoPromise = uploadPhoto();
   const createUserPromise = createUser();
-  let text = '';
-  let arr = [];
 
   Promise.all([uploadPhotoPromise, createUserPromise])
-    .then((values) => {
-      const val = values.map(Object.values);
-      arr = val[0].concat(val[1]);
-      text = arr.slice(1).join(' ');
-      console.log(text);
+    .then(([upload, user]) => {
+      console.log(`${upload.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
