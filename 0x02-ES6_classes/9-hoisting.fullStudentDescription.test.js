eb726b9180ccc512f04028337abf096848b1c6d1
MercyKorir <mcthegreat78@gmail.com>
2023-06-30 12:11:06 +0300

    I have made corrections to hoisting file

---
+++ 0x02-ES6_classes/9-hoisting.fullStudentDescription.test.js
@@ -0,0 +1,9 @@
+import listOfStudents, {
+  StudentHolberton,
+  HolbertonClass,
+} from './9-hoisting.js';
+
+test('initialization works correctly', () => {
+  expect(listOfStudents[0] instanceof StudentHolberton).toBe(true);
+  expect(listOfStudents[0].holbertonClass instanceof HolbertonClass).toBe(true);
+});
