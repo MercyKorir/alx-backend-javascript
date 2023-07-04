45acbd336543c8993d8ee2f084e496615ad23f53
MercyKorir <mcthegreat78@gmail.com>
2023-07-04 21:37:41 +0300

    I have added the file for task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   let res = '';
-  if (startString === '') return '';
+  if (startString === '' || typeof startString !== 'string') return '';
   for (const element of set) {
     if (element.startsWith(startString)) {
       res += `-${element.slice(startString.length)}`;
