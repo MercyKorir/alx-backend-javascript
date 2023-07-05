210da9c55542501b98072cb24e7ba6b59ed0c029
MercyKorir <mcthegreat78@gmail.com>
2023-07-05 10:50:40 +0300

    I have fixed the undefined error in task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let res = '';
   if (startString === '' || typeof startString !== 'string') return '';
   for (const element of set) {
-    if (element.startsWith(startString)) {
+    if (element !== undefined && element.startsWith(startString)) {
       res += `-${element.slice(startString.length)}`;
     }
   }
