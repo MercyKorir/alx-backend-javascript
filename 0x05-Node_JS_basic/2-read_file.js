18d9bdb4f34a0b36fb7e1ba47b276f862297a09f
MercyKorir <mcthegreat78@gmail.com>
2023-09-20 09:21:35 +0300

    I have added the file for task 2*

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const studentsByField = {};
+    let totalStudents = 0;
+
+    for (const line of lines) {
+      const [firstName, lastName, age, field] = line.split(',');
+
+      if (field !== 'field') {
+        totalStudents++;
+        if (studentsByField[field]) {
+          studentsByField[field].push(firstName);
+        } else {
+          studentsByField[field] = [firstName];
+        }
+      }
+    }
+    console.log(`Number of students: ${totalStudents}`);
+    for (const field in studentsByField) {
+      const studentsList = studentsByField[field].join(', ');
+      console.log(
+        `Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsList}`,
+      );
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
